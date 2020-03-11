e60a5b0b9cde876a2de255fdb86d452ac713918b
William Gaylord <chibill110@gmail.com>
2020-03-11 17:53:37 -0500

    Added the generic opcodes as fucntions in cpu.py

--- emulator/cpu.py
+++ emulator/cpu.py
@@ -1,4 +1,5 @@
 import memory
+import time
 import instructions
 
 class CPU:
@@ -9,4 +10,87 @@ class CPU:
     def tick(self):
         pass
     
+    def GHALT(self):
+        while(1):
+            time.sleep(1)
+
+    def GLD(self):
+        pass
         
+    def GST(self):
+        pass
+   
+    def GLI(self):
+        pass
+        
+    def GLR(self):
+        pass
+        
+    def GSR(self):
+        pass
+        
+    def GADD(self):
+        pass
+        
+    def GSUB(self):
+        pass
+        
+    def GSFR(self):
+        pass
+        
+    def GCMP(self):
+        pass
+        
+    def GXOR(self):
+        pass
+        
+    def GAND(self):
+        pass
+
+    def GOR(self):
+        pass
+        
+    def GPSH(self):
+        pass
+        
+    def GPOP(self):
+        pass
+        
+    def GMV(self):
+        pass
+
+    def GBRGI(self):
+        pass
+        
+    def GBRL(self):
+        pass
+        
+    def GBRE(self):
+        pass
+        
+    def GBRP(self): 
+        pass
+        
+    def GBRN(self):
+        pass
+        
+    def GJMP(self):
+        pass
+        
+    def GSUB(self):
+        pass
+        
+    def GSRET(self):
+        pass
+        
+    def GINTE(self):
+        pass
+        
+    def GINTD(self):
+        pass
+        
+    def GINTD(self):
+        pass
+        
+    def GIRET(self):
+        pass
